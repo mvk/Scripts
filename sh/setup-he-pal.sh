ed7209ef4c1a8feade9470840350108c590bb715
Max Kovgan <max@kovgans.online>
2026-09-11 12:34:33 +0300

    Update paleo heb setup
    Signed-off-by: Max Kovgan <max@kovgans.online>

--- sh/setup-he-pal.sh
+++ sh/setup-he-pal.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")" &>/dev/null
+SCRIPT_DEBUG="${SCRIPT_DEBUG:-0}"
+SCRIPT_FORCE="${SCRIPT_FORCE:-0}"
+SCRIPT_ID="${SCRIPT_ID:-"${SCRIPT_NAME%%.*}-$(date +%s || true)"}"
+if [[ -d "${SCRIPT_DIR}/lib" ]]; then
+  for fname in "${SCRIPT_DIR}/lib"/*.bash; do
+    [[ -r "${fname}" ]] || {
+      echo "WARN: failed to read ${fname}"
+      continue
+    }
+    # shellcheck source=lib/base.bash
+    source "${fname}"
+  done
+fi
 declare -a PACKAGES
 declare -a CURR_CMD
 
@@ -18,22 +33,21 @@ fi
 LAYOUTS_CURRENT="${LAYOUTS_CURRENT:-"$(kreadconfig6 --file kxkbrc --group Layout --key LayoutList || true)"}"
 VARIANTS_CURRENT="${VARIANTS_CURRENT:-"$(kreadconfig6 --file kxkbrc --group Layout --key VariantList || true)"}"
 
-echo "LAYOUTS_CURRENT: '${LAYOUTS_CURRENT}'"
-echo "VARIANTS_CURRENT: '${VARIANTS_CURRENT}'"
+log.debug "LAYOUTS_CURRENT: '${LAYOUTS_CURRENT}'"
+log.debug "VARIANTS_CURRENT: '${VARIANTS_CURRENT}'"
 
 CURR_CMD=()
 [[ "${CURR_UID}" -ne 0 ]] && CURR_CMD+=(sudo)
 CURR_CMD+=(dnf install -y)
 CURR_CMD+=("${PACKAGES[@]}")
 
-echo "CURR_CMD: '${CURR_CMD[*]}'"
+log.debug "CURR_CMD: '${CURR_CMD[*]}'"
 "${CURR_CMD[@]}"
 
 SRC="${SRC:-"he-pal"}"
 DEST="${XKB_SYMBOLS_TARGET}"
 
 CURR_CMD=()
-[[ "${CURR_UID}" -ne 0 ]] && CURR_CMD+=(sudo)
 CURR_CMD+=(cp "xkb/symbols/${SRC}" "${DEST}/")
 
 echo "CURR_CMD: '${CURR_CMD[*]}'"
@@ -41,39 +55,46 @@ echo "CURR_CMD: '${CURR_CMD[*]}'"
 LAYOUT_NEW="${LAYOUT_NEW:-"he-pal"}"
 VARIANT_NEW="${VARIANT_NEW:-""}"
 
-if [[ "${LAYOUTS_CURRENT}" =~ *$LAYOUT_NEW* ]]; then
-  echo "Found the layout ${LAYOUT_NEW} is already in ${LAYOUTS_CURRENT}"
-  exit 0
+if [[ ! ",${LAYOUTS_CURRENT}," =~ ^.*$LAYOUT_NEW.*$ ]]; then
+  declare -A ITEMS
+  ITEMS['LayoutList']="${LAYOUTS_CURRENT},${LAYOUT_NEW}"
+  ITEMS['VariantList']="${VARIANTS_CURRENT},${VARIANT_NEW}"
+  for key in "${!ITEMS[@]}"; do
+    CURR_CMD=()
+    CURR_CMD+=(
+      kwriteconfig6
+      --file kxkbrc
+      --group Layout
+      --key "${key}" "${ITEMS[${key}]}"
+      --notify
+    )
+    log.debug "CURR_CMD: '${CURR_CMD[*]}'"
+    "${CURR_CMD[@]}"
+    RC=$?
+    log.info "Updated ${key} with: ${ITEMS[${key}]} and RC=${RC}"
+  done
+else
+  log.warn "Found the layout ${LAYOUT_NEW} is already in ${LAYOUTS_CURRENT}"
 fi
-# 4. Write the updated strings back to your KDE configuration with notify flags
-CURR_CMD=()
-CURR_CMD+=(
-  kwriteconfig6
-  --file kxkbrc
-  --group Layout
-  --key LayoutList "${LAYOUTS_CURRENT},${LAYOUT_NEW}"
-  --notify
-)
-echo "CURR_CMD: '${CURR_CMD[*]}'"
-"${CURR_CMD[@]}"
-
-CURR_CMD=()
-CURR_CMD+=(
-  kwriteconfig6
-  --file kxkbrc
-  --group Layout
-  --key VariantList "${VARIANTS_CURRENT},${VARIANT_NEW}"
-  --notify
-)
-echo "CURR_CMD: '${CURR_CMD[*]}'"
-"${CURR_CMD[@]}"
 # 5. Force KWin to reload the hardware inputs immediately
-
 CURR_CMD=()
 CURR_CMD=(
   qdbus-qt6
   org.kde.KWin
   /KWin org.kde.KWin.reconfigure
 )
-echo "CURR_CMD: '${CURR_CMD[*]}'"
+log.debug "CURR_CMD: '${CURR_CMD[*]}'"
 "${CURR_CMD[@]}"
+RC=$?
+log.debug "Refreshed kwin returned RC=${RC}"
+
+SRC="fontconfig/fonts.conf"
+DST="${HOME}/.config"
+
+mkdir -p "${DST}/${SRC%/*}"
+cp "${SRC}" "${DST}/${SRC}"
+
+fc-cache -vf
+RC=$?
+
+exit "${RC}"

7e6d6a9a04fce9f719009940973b655bab1ba91a
Maxim Kovgan <max@kovgans.online>
2026-09-18 10:04:14 +0300

    Working mac
    Signed-off-by: Maxim Kovgan <max@kovgans.online>

--- sh/.env.setup_emacs.doom.bash
+++ sh/.env.setup_emacs.doom.bash
@@ -10,3 +10,10 @@ EMACS_CFG_FILES=()
 EMACS_CFG_FILES["init"]="${HOME}/.config/doom/init.el,${PWD}/doom/init.el.j2,${PWD}/doom/init.context.yaml"
 EMACS_CFG_FILES["config"]="${HOME}/.config/doom/config.el,${PWD}/doom/config.el.j2,${PWD}/doom/config.context.yaml"
 EMACS_CFG_FILES["packages"]="${HOME}/.config/doom/packages.el,${PWD}/doom/packages.el.j2,${PWD}/doom/packages.context.yaml"
+
+## Mac specifig
+SETUP_QUICKLISP_SKIP=1
+SETUP_SBCLRC_FILE_SKIP=1
+SETUP_DESKTOP_FILE_SKIP=1
+SETUP_PLASMA_HOOKS_SKIP=1
+
